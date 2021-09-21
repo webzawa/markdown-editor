243dcdb470700d1245c8d7392dbb3075e1172129
webzawa <35700309+webzawa@users.noreply.github.com>
2021-09-21 12:31:26 +0900

    webpack-dev-server を導入して開発効率を上げる

--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: 'dist/',
+  },
+  devServer: {
+    publicPath: '/dist/',
+    hot: true,
   }
 }
