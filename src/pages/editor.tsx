429b8369bcb398b17d8f294b6b95c7e92e28962d
webzawa <35700309+webzawa@users.noreply.github.com>
2021-09-23 06:51:23 +0900

    マークダウンのプレビューを作る

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import { useStateWithStorage } from "../hooks/use_state_with_storage";
+import * as ReactMarkdown from "react-markdown";
 
 const StorageKey = 'pages/editor:text'
 
@@ -59,7 +60,9 @@ export const Editor: React.FC = () => {
           onChange={(event) => setText(event.target.value)}
           value={text}
         />
-        <Preview>プレビューエリア</Preview>
+        <Preview>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Preview>
       </Wrapper>
     </>
   )
