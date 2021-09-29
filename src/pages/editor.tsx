1a870c5682fe6a076a6f6267c263fc1fa090c4b6
webzawa <35700309+webzawa@users.noreply.github.com>
2021-09-29 21:47:22 +0900

    モーダルを出して保存時のタイトルを指定できるようにする

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -4,6 +4,9 @@ import { useStateWithStorage } from "../hooks/use_state_with_storage";
 import * as ReactMarkdown from "react-markdown";
 import { putMemo } from "../indexeddb/memos";
 import { Button } from "../components/button";
+import { SaveModal } from "../components/save_modal";
+
+const { useState } = React
 
 const StorageKey = 'pages/editor:text'
 
@@ -61,16 +64,14 @@ const Preview = styled.div`
 export const Editor: React.FC = () => {
   const [text, setText] = useStateWithStorage('', StorageKey)
 
-  const saveMemo = (): void => {
-    putMemo('TITLE', text)
-  }
+  const [showModal, setShowModal] = useState(false)
 
   return (
     <>
       <Header>
         Markdown Editor
         <HeaderControl>
-          <Button onClick={saveMemo}>
+          <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
         </HeaderControl>
@@ -84,6 +85,15 @@ export const Editor: React.FC = () => {
           <ReactMarkdown>{text}</ReactMarkdown>
         </Preview>
       </Wrapper>
+      {showModal && (
+        <SaveModal
+          onSave={(title: string): void => {
+            putMemo(title, text)
+            setShowModal(false)
+          }}
+          onCancel={() => setShowModal(false)}
+        />
+      )}
     </>
   )
 }
